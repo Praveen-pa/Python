cf36715aa02cb40568aa36c1888b28db932b3922
Praveen Annadurai <112604134+Praveen-pa@users.noreply.github.com>
2022-09-21 11:44:13 +0530

    Update Add binary.py

--- Add binary.py
+++ Add binary.py
@@ -1,8 +1,13 @@
-A=int(input("a="),2)
-B=int(input("b="),2)
-if A == 0 or 1 and B == 0 or 1 :
-  c=bin(A+B)
-  d=c[2:]
-  print(d)
+A=input("a=")
+B=input("b=")
+p=set(A)
+x=set(B)
+s={'0','1'}
+if s == p or p == {'0'} or p == {'1'} and s ==x or x == {'0'} or x == {'1'} :
+    A=int(A,2)
+    B=int(B,2)
+    c=bin(A+B)
+    d=c[2:]
+    print(d)
 else:
-  print("Enter only 0 or 1 ")
+    print("Enter only 0 or 1 ")

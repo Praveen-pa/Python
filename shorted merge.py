38fe4968bb8d9705dc0aa31b8d0e11d38f30892c
Praveen Annadurai <112604134+Praveen-pa@users.noreply.github.com>
2022-09-21 10:10:51 +0530

    Update shorted merge.py

--- shorted merge.py
+++ shorted merge.py
@@ -1,22 +1,15 @@
 l1 = []
 l2 = []
-
-print("Enter element for list1: ")
-while True:
+a=int(input("Enter the size of list 1 :"))
+for i in range(0,a):
+  print("Enter element for list1: ")
   s = input()
-  if s == '*':
-    break
-  else:
-    l1.append(int(s))
-
-print("Enter element for list2: ")
-while True:
+  l1.append(int(s))
+b=int(input("Enter the size of list 2 :"))
+for j in range(0,b):
+  print("Enter element for list2: ")
   s = input()
-  if s == '*':
-    break
-  else:
-    l2.append(int(s))
-
+  l2.append(int(s))
 l1.extend(l2)
 l1.sort()
 print(l1)

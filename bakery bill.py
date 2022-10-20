dfe142098b38f149160edebbd575e6400982e4c4
Praveen Annadurai <112604134+Praveen-pa@users.noreply.github.com>
2022-10-20 09:29:03 +0530

    Update bakery bill.py

--- bakery bill.py
+++ bakery bill.py
@@ -1,6 +1,6 @@
 a=int(input("Enter the Number of new loaves purchased : "))
 b=int(input("Enter the Number of old loaves purchased : "))
-c=(185-(0.6*185))*b
+c=(185*185)*b
 d=185*a
 print("Regular Price : Rs. 185")
 print("Old loaves Amount : Rs. ",c)
